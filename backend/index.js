7c16bb8ac91bef120ac870693803f21750c976ed
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-07-03 11:31:47 +0530

    perf: error handling part is fixed if open weather map api doesn't give data

--- backend/index.js
+++ backend/index.js
@@ -13,22 +13,32 @@ app.get('/api/weather', async (req, res) => {
   try {
     const fileContent = fs.readFileSync('./cities.json', 'utf8');
     const cities = JSON.parse(fileContent);
-    let cityCodes = [];
-    cityCodes = cities.List.map(city => city.CityCode);
-    const weatherData = await Promise.all(
-      cityCodes.map(id => fetch(
-        `${OPEN_WEATHER_MAP_URL}?id=${id}&units=metric&appid=${API_KEY}`
-      ).then(res => res.json()))
-    )
-    return res.json({ source: 'api', data: weatherData });
+    const cityCodes = cities.List.map(city => city.CityCode);
+
+    const response = await Promise.all(
+      cityCodes.map(async id => {
+        try {
+          const res = await fetch(`${OPEN_WEATHER_MAP_URL}?id=${id}&units=metric&appid=${API_KEY}`);
+          if (!res.ok) {
+            throw new Error(`Failed to fetch data for city ID ${id}`);
+          }
+          return await res.json();
+        } catch (err) {
+          console.error(`Error for city ID ${id}:`, err.message);
+          return null;
+        }
+      })
+    );
+
+    return res.json({ source: 'api', data: response.filter(r => r !== null) });
   } catch (error) {
     return res.status(500).json({
       message: 'There is an error in the server',
       error: error.message
-    })
+    });
   }
-}
-)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
